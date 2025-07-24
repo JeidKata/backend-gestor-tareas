09068a48869f3404084e1abea7231b0916e1e6de
JeidyOlaya <jeidkata@gmail.com>
2025-07-24 08:21:30 -0500

    Agrego tabla que tiene las diferentes fases.

---
+++ src/models/fase_m.py
@@ -0,0 +1,17 @@
+from models import Base
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+class Fase(Base):
+    __tablename__ = "fase"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False, unique=True) # e.g., "Planning", "Development", "Testing"
+
+
+    # Relationship to Tarea (One-to-Many: One Fase can have many Tareas)
+    tareas = relationship("Tarea", back_populates="fase_asociada")
+
+    def __repr__(self):
+        return f"<Fase(id={self.id}, nombre='{self.nombre}')>"
+    
