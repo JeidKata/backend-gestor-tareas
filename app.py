c968ed04d64ef4012afb262044f7925d24ded1c4
JeidyOlaya <jeidkata@gmail.com>
2025-07-24 08:52:00 -0500

    Importo Flask-Cors y corrijo la importación de models.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from src.model import Base, engine
+from models import Base, engine
 from flask_controller import FlaskControllerRegister
+from flask_cors import CORS
 
 app = Flask(__name__)
 
