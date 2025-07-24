88786219a8201c4ee5d36635d936d1b1932b0471
JeidyOlaya <jeidkata@gmail.com>
2025-07-24 07:52:27 -0500

    Inicializar aplicación Flask con configuración básica - Agregar configuración CORS - Establecer clave secreta para sesiones - Habilitar modo debug

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from src.model import Base, engine
+from flask_controller import FlaskControllerRegister
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "¡Aplicación Flask corriendo correctamente!"
+
+#se agrega el cors a la app y se configura para que solo acepte peticiones con el header Content-Type
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+#se crea la llave para la sesion
+app.secret_key = 'llave_para_sesion'
+app.debug = True
+
+register = FlaskControllerRegister(app)
+register.register_package("src.controller")
+
+Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    app.run(debug=True)
