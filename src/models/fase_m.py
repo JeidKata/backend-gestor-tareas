613975d5f9f77507957affb077afd549aa3d5caa
JeidyOlaya <jeidkata@gmail.com>
2025-07-24 11:13:31 -0500

    refactor: Cambio el import de Base - Agrego constructor y to_dict()

--- src/models/fase_m.py
+++ src/models/fase_m.py
@@ -1,4 +1,4 @@
-from models import Base
+from . import Base
 from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.orm import relationship
 
@@ -11,6 +11,15 @@ class Fase(Base):
 
     # Relationship to Tarea (One-to-Many: One Fase can have many Tareas)
     tareas = relationship("Tarea", back_populates="fase_asociada")
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+        }
 
     def __repr__(self):
         return f"<Fase(id={self.id}, nombre='{self.nombre}')>"
