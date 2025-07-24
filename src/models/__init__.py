82ec01bb6bfdede5baa9a5e4c7d28963f434e5b9
JeidyOlaya <jeidkata@gmail.com>
2025-07-24 14:22:28 -0500

    refactor: Quito importaciones que no uso, y reorganizo el código, creando la variable session.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,10 +1,12 @@
-from datetime import datetime
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./my_project.db"
 engine = create_engine(DATABASE_URL)
+conexion = engine.connect()
+
+Base = declarative_base()
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+session = SessionLocal()
